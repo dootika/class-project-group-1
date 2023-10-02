6c8ff5ddcba2ce740c1786d04df4de825564d8d9
luthanhar <82824823+luthanhar@users.noreply.github.com>
2023-10-03 05:08:24 +0530

    Update scrap_ARI_deaths.R

--- scrap_ARI_deaths.R
+++ scrap_ARI_deaths.R
@@ -2,7 +2,7 @@
 library(readxl)
 library(openxlsx)
 # Problem 1
-ARI_death=read_excel("/Users/luthanhar/Downloads/no_deaths_2011.xlsx")
-World_AQI = read_excel("/Users/luthanhar/Downloads/aap_pm_database_may2014.xls")
+ARI_death=read_excel("no_deaths_2011.xlsx")
+World_AQI = read_excel("aap_pm_database_may2014.xls")
 
 
