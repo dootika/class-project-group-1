df14b22e1ddbc26b1589751dc34439c0bc0d57b9
unknown <thpcgmr96@gmail.com>
2023-11-08 21:50:09 +0530

    update

--- Code/scrap_ARI_deaths.R
+++ Code/scrap_ARI_deaths.R
@@ -22,5 +22,34 @@ x <- as.numeric(ARI_death[16,2:7])
 y <- as.numeric(ARI_death[15,2:7])
 ARI_death[15,] = c("Jammu and Kashmir" , x+y)
 ARI_death <- ARI_death[-16,]
-write.csv(ARI_death , file = "Deaths_2011.csv")
+ARI_death[5,"State"] = "Bihar"
 
+
+#adding populations
+
+library(dplyr)
+library(rvest)
+
+html <- read_html("https://en.wikipedia.org/wiki/List_of_states_and_union_territories_of_India_by_population")
+pop <- html_table(html)
+
+pop <- pop[[2]]
+pop <- as.data.frame(pop)
+head(pop)
+State <- pop[-1,2]
+populations <- pop[-1,3]
+State[33] = "Dadra and Nagar Haveli"
+populations[33] = 343709
+State[38] = "Daman and Diu"
+populations[38] = 243247
+populations <- as.numeric(gsub(",","",populations))
+populations[10] <-49577103
+
+state_populations <- data.frame(State, populations)
+unique(temp$State)
+states[19] = "Delhi"
+states[25] = "Manipur"
+
+Deaths_2011 <- merge(ARI_death , state_populations)
+
+write.csv(Deaths_2011 , "Deaths_2011.csv")
